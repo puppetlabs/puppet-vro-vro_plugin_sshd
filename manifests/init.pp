268305bdfbcc48a4d163e127f3bf2a26966c1988
tspeigner <tspeigner@gmail.com>
2016-11-30 13:43:12 -0800

    removed ruby-augeas module requirement.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,13 +2,9 @@
 #### https://forge.puppet.com/herculesteam/augeasproviders_ssh
 ## Include augeasproviders_core from forge or github.
 #### https://github.com/hercules-team/augeasproviders_core
-## Requirements for Augeas
-#### Ensure both Augeas and ruby-augeas 0.3.0+ bindings are installed and working as normal.
 
 class vro_plugin_sshd {
-
-#include vro_plugin_sshd::ruby_augeas
-
+  
   ## If you are using root then enable root login, else use vro-plugin_user created by vro-plugin.
   sshd_config { "PermitRootLogin":
     ensure => present,
